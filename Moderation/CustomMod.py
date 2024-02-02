ebd61b4b63069cad7fb3b86f75e4667e50c5b6ad
adarsh2701 <adarsharyan2701@gmail.com>
2024-02-02 17:37:08 +0530

    Added proper options in SelectMenu and added one trial Modal. Also added a few notions in BasicMod.py

--- Moderation/CustomMod.py
+++ Moderation/CustomMod.py
@@ -1,7 +1,7 @@
 import logging
 import traceback
 import json
-from interactions import Extension, Modal, ShortText, slash_command, InteractionContext, subcommand, ParagraphText, StringSelectMenu, StringSelectOption, ActionRow, Button, ButtonStyle, Embed, Color, component_callback, ComponentContext, listen
+from interactions import Extension, component_callback, Modal, ShortText, slash_command, InteractionContext, subcommand, ParagraphText, StringSelectMenu, StringSelectOption, ActionRow, Button, ButtonStyle, Embed, Color, component_callback, ComponentContext, listen
 from Resources.CustomEmojis import *
 from interactions.api.events import GuildJoin
 # server_id,custom_ban_message_dms,custom_kick_message_dms,custom_timeout_message_dms,custom_soft_ban_message_dms,custom_warn_message_dms
@@ -76,19 +76,31 @@ class CustomMod(Extension):
             print(f"dm_on_kick: {dm_on_kick}")
             print(f"custom_kick_message_dms: {custom_kick_message_dms}")
 
-
+            global kick_list_value
+            global dm_kick_list_value
+            global dm_kick_custom_list_value
+            
             if custom_kick_message == "None":
                 option_type = f"{disabled_check}{enabled_cross}"
+                kick_list_value = "Enable"
+                
             else:
                 option_type = f"{enabled_check}{disabled_cross}"
+                kick_list_value = "Disable"
             if dm_on_kick == "False":
                 option_type = f"{disabled_check}{enabled_cross}"
-                sub_option_type = f"{disabled_check}{disabled_cross}"
+                sub_option_type = f"{disabled_check}{disabled_cross}"   
+                dm_kick_list_value = "Enable"
+            
             else:
                 option_type = f"{enabled_check}{disabled_cross}"
                 sub_option_type = f"{disabled_check}{enabled_cross}"
+                dm_kick_list_value = "Disable"
+                dm_kick_custom_list_value = "Enable"
+                
                 if custom_kick_message_dms != "None":
                     sub_option_type = f"{enabled_check}{disabled_cross}"
+                    dm_kick_custom_list_value = "Disable"
         
             customize_kick_command_embed = Embed(
                 title = "Customize the Kick Command!",
@@ -96,7 +108,7 @@ class CustomMod(Extension):
                 color = Color.from_hex("#000000")
             )
             customize_kick_command_embed.add_field(
-                name = f"> Custom Kick Message          {option_type}",
+                name = f"> Custom Kick Message           {option_type}",
                 value= "Have me send a message of your choice \nwhen a user is kicked!\n\u200b"
             )
             customize_kick_command_embed.add_field(
@@ -111,19 +123,47 @@ class CustomMod(Extension):
                 name = "\u200b",
                 value ="Select the modifications you want to apply\nusing the drop down below."
             )
-            # TODO: Make the drop down dynamic
-            options = [
-                StringSelectOption(label="option 1", value="1"),
-                StringSelectOption(label="option 2", value="2"),
-                StringSelectOption(label="option 3", value="3")
-            ]
+            if dm_on_kick == "False":
+                options = [
+                    StringSelectOption(label=f"{kick_list_value} Custom Kick Message", value= "1"),
+                    StringSelectOption(label=f"{dm_kick_list_value} DM User on Kick", value= 4)
+                ]
+            else:
+                options = [
+                    StringSelectOption(label=f"{kick_list_value} Custom Kick Message", value= 1),
+                    StringSelectOption(label=f"{dm_kick_list_value} DM User on Kick", value= 2),
+                    StringSelectOption(label=f"{dm_kick_custom_list_value} Custom DM Kick Message", value= 3)
+                ]
+            
             kick_customs_menu = StringSelectMenu(
                 *options,          
                 custom_id= 'kick_custom_list',
                 placeholder= 'Select a modification!',
                 max_values=1, min_values=1)
             
-            await ctx.send(embeds=customize_kick_command_embed, components=kick_customs_menu)
+            global kick_custom_modal
+            kick_custom_modal = Modal(
+                ParagraphText(
+                    label="Enter your kick message!",
+                    custom_id="text_kick_custom",
+                    value = "Code words:\n1. {server_name} : Name of the server\n"\
+                        "2. {user_name} : Name of user\n"\
+                        "3. {member_count} : Number of Server Members\n"\
+                        "4. {author_name} : Name of the user who used the command\n"\
+                            "All markdowns available in discord work.\n\n"\
+                                "Example Text: Aaaand Yeetus Burgertus people! **{user_name}** has been kicked from {server_name}! That leaves us with {member_count} peeps!",),
+                title="Customize Kick!",    
+                custom_id="kick_custom_model",
+            )
 
+            await ctx.send(embeds=customize_kick_command_embed, components=kick_customs_menu, ephemeral= True)
+        
         except Exception as e:
             await self.error_handler(e, ctx)
+
+    @component_callback("kick_custom_list")
+    async def kick_custom_list(self, ctx: ComponentContext):
+        selected_option = ctx.values[0]
+        if selected_option == "1" and kick_list_value == "Enable":
+            await ctx.send_modal(modal = kick_custom_modal)
+            # TODO: Make an option for embed messages and stuff for other options
